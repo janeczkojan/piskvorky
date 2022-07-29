7c149b0cef4df3bc9554ae445a5bbc89962068bd
janeczkojan <32244922+janeczkojan@users.noreply.github.com>
2022-07-29 12:03:46 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,8 @@ import type { Player } from './types';
 
 
 export const DEFAULT_PLAYERS: Player[] = [
-	{ id: 1, symbol: 'x', textColor: 'white', backgroundColor: 'red' },
-	{ id: 2, symbol: 'o', textColor: 'purple', backgroundColor: 'lightyellow' }
+	{ id: 1, symbol: '卐', textColor: 'white', backgroundColor: 'red' },
+	{ id: 2, symbol: '✡', textColor: 'purple', backgroundColor: 'lightyellow' }
 ];
 
 
